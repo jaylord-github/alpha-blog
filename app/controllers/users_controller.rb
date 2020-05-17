97b3b7f3610eb6e8b2892e31738df565208060e5
deumon <sio.donayre@go-va.com.au>
2020-05-17 17:38:55 +0800

    added permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
+    @users = User.paginate(page: params[:page], per_page: 2)
   end
 
   def new
@@ -10,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up."
       redirect_to articles_path
     else
@@ -18,27 +23,42 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
-  def edit
-    @user = User.find(params[:id])
+  def edit    
   end
 
-  def update
-    @user = User.find(params[:id])
+  def update    
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated."
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
   end
 
+  def destroy
+      @user.destroy
+      session[:user_id] = nil if @user == current_user
+      flash[:notice] = "Account and all associated articles successfullly deleted"
+      redirect_to articles_path
+  end
+
   private
+
+  def load_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit your own profile."
+      redirect_to @user
+    end
   end
 end
